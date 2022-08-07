f216a3965bb76ff57427bae34e5c69df4be22b93
markflerko <markflerko@gmail.com>
2022-08-07 23:39:14 +0400

    feat: localauth body check

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    const { login, password } = context.switchToHttp().getRequest().body;
+
+    if (
+      login === null ||
+      login === null ||
+      typeof login !== 'string' ||
+      password === null ||
+      password === null ||
+      typeof password !== 'string'
+    ) {
+      throw new BadRequestException([
+        'login should not be empty',
+        'login must be a string',
+        'password should not be empty',
+        'password must be a string',
+      ]);
+    }
+
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
